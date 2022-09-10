54fab7c28b3d6d44ca5e7ddd320ff04cd0e6efd9
Aikaso <aikasoinc@gmail.com>
2022-09-10 20:28:06 +0200

    firstone

---
+++ src/components/Categories.jsx
@@ -0,0 +1,12 @@
+import { Container } from '@material-ui/core'
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+export default Categories
